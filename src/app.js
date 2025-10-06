31cb640914699dc9f6a4808737f01fb815b10598
cesarodriguezvelas <cesaralejandrodriv@gmail.com>
2025-10-06 17:08:47 +0000

    First Commit

--- src/app.js
+++ src/app.js
@@ -6,6 +6,30 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+    let quien = ['Mi uña', 'Mi perro', 'Mi abuela', 'El vecino', 'Mi hermano', 'El clima'];
+    let queHizo = ['se lastimo', 'se enfermó', 'se tropezo', 'llamó urgentemente', 'necesitó ayuda'];
+    let cuando = ['en la mañana', 'hace un momento', 'justo ahora', 'antes de salir', 'mientras almorzaba'];
+    let evitar = 'y no puedo lavar los platos';
+
+    function obtenerElementoAleatorio(array) {
+        let indiceAleatorio = Math.floor(Math.random() * array.length);
+        return array[indiceAleatorio];
+    } 
+
+    function generarExcusa() {
+        let excusa = obtenerElementoAleatorio(quien) + " " +
+                     obtenerElementoAleatorio(queHizo) + " " +
+                     obtenerElementoAleatorio(cuando) + " " +
+                     evitar;
+        
+        return excusa;
+    }
+
+    function iniciarGenerador() {
+        let elementoExcusa = document.getElementById('excusa');
+        let excusaGenerada = generarExcusa();
+        elementoExcusa.textContent = excusaGenerada;
+    }
+
+    iniciarGenerador();
 };
